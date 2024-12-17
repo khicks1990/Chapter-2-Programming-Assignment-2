dfd4679ee2f1a7209cb72f3dbf8bdfda766b48c5
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-17 06:56:35 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,3 @@
+# Instructions  
+
+Write a program that asks the user to enter three test scores. The program should display each test score, as well as the average of the scores.
---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // Needed for the Scanner class
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      //variables
+             
+      // Create a Scanner object for keyboard input.
+
+      // Get the first test score.
+      
+      // Get the second test score.
+      
+      // Get the first test score.
+
+      // Calculate the average.
+      
+      // Display the average.
+   }
+}
